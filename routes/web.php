3b8aeb969d06bc890cd54c99ef11975e664c2e84
Brandon Villa <bornofos@gmail.com>
2017-07-28 13:19:35 -0500

    Views added to routing file

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,75 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
-Route::get('prueba', function() {
-    return view('layouts.master');
+Route::get('/busqueda', function () {
+    return view('busqueda');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/signup', function () {
+    return view('signup');
+});
+Route::get('/condiciones-de-uso', function () {
+    return view('condiciones-de-uso');
+});
+Route::get('/acerca-de-nosotros', function () {
+    return view('acerca-de-nosotros');
+});
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+Route::get('/politicas-de-privacidad', function () {
+    return view('politicas-de-privacidad');
+});
+Route::get('/preguntas-frecuentes-sobre-tiempos-compartidos', function () {
+    return view('preguntas-frecuentes-sobre-tiempos-compartidos');
+});
+Route::get('/mi-cuenta', function () {
+    return view('mi-cuenta');
+});
+Route::get('/mis-datos', function () {
+    return view('mis-datos');
+});
+Route::get('/mis-membresias', function () {
+    return view('mis-membresias');
+});
+Route::get('/mis-mensajes', function () {
+    return view('mis-mensajes');
+});
+Route::get('/mis-favoritos', function () {
+    return view('mis-favoritos');
+});
+Route::get('/new-membresia', function () {
+    return view('new-membresia');
+});
+Route::get('/edit-membresia/{id}', function () {
+    return view('edit-membresia');
+});
+Route::get('/mi-cuenta/membresia-ubicacion/{id}', function () {
+    return view('membresia-ubicacion');
+});
+Route::get('/membresia/{titulo}/{id}', function () {
+    return view('membresia-ubicacion');
+});
+Route::get('/promociones', function () {
+    return view('promociones');
+});
+Route::get('/promociones/{titulo}/{id}', function () {
+    return view('promociones-detail');
+});
+Route::get('/listados', function () {
+    return view('listados');
+});
+Route::get('/listados/{categoria}/{titulo}', function () {
+    return view('listados-categoria');
+});
+Route::get('/listados/{categoria}/{subcategoria}/{titulo}', function () {
+    return view('listados-detail');
+});
+Route::get('/concepto-de-tiempo-compartido', function () {
+    return view('concepto-de-tiempo-compartido');
 });
