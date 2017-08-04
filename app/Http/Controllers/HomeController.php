a496449ca5546c8409c734be0de6401825824a33
Brandon Villa <bornofos@gmail.com>
2017-08-04 10:24:08 -0500

    Temporal commit ignored

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,21 +15,21 @@ class HomeController extends Controller
      */
     public function index()
     {
-        // // Get the instance to make HTTP Requests        
-        // $client = User::getClient();
-        // try {
-        //     // Get all membresias
-        //     $response = Membresia::getMembresias($client);
-        // } catch (RequestException $e) {
-        //     // In case something went wrong it will redirect to home page
-        //     return view('home.index');
-        // }
+        // Get the instance to make HTTP Requests        
+        $client = User::getClient();
+        try {
+            // Get all membresias
+            $response = Membresia::getMembresias($client);
+        } catch (RequestException $e) {
+            // In case something went wrong it will redirect to home page
+            return view('home.index');
+        }
         
-        // // Get the response body from HTTP Request and parse to Object        
-        // $membresias = json_decode($response->getBody()->getContents());
+        // Get the response body from HTTP Request and parse to Object        
+        $membresias = json_decode($response->getBody()->getContents());
         
-        // // Return to home view with the Object: $membresias
-        return view('home.index');
+        // Return to home view with the Object: $membresias
+        return view('home.index', compact('membresias'));
 
     }
 
