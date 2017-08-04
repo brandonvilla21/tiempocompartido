c42b09dc0cefcf18ef971bec2c0b4d7a66d78890
Brandon Villa <bornofos@gmail.com>
2017-08-04 09:42:31 -0500

    Now HomeController calls their views from a proper route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ use App\Membresia;
 class HomeController extends Controller
 {
     /**
+     * Display a listing of Membresias
      *
      * @return \Illuminate\Http\Response
      */
@@ -17,18 +18,18 @@ class HomeController extends Controller
         // Get the instance to make HTTP Requests        
         $client = User::getClient();
         try {
-            // Get all the membresias
+            // Get all membresias
             $response = Membresia::getMembresias($client);
         } catch (RequestException $e) {
             // In case something went wrong it will redirect to home page
-            return view('home');
+            return view('home.index');
         }
         
         // Get the response body from HTTP Request and parse to Object        
         $membresias = json_decode($response->getBody()->getContents());
         
         // Return to home view with the Object: $membresias
-        return view('home', compact('membresias'));
+        return view('home.index', compact('membresias'));
 
     }
 
