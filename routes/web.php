a6fdaaa573ea99aa7db9a0695d3a8739ceca373b
Brandon Villa <bornofos@gmail.com>
2017-08-08 09:41:23 -0500

    /cambiar-contrasena route added and handled from UserController

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::post('/signup', 'RegistrationController@store');
 
 // USER
 Route::get('/mis-datos', 'UserController@edit');
+Route::get('/cambiar-contrasena', 'UserController@editPassword');
 Route::put('/guardar-datos', 'UserController@update');
 
 // MEMBRESIAS
