2b805fa1c0ab056a538e2404dd9f67fe1d2d159d
Brandon Villa <bornofos@gmail.com>
2017-08-09 14:46:58 -0500

    edit function added

--- app/Membresia.php
+++ app/Membresia.php
@@ -38,6 +38,73 @@ class Membresia extends Model
                 'ocultarFecha'          => isset($request->ocultarFecha),
                 'compraCaduca'          => isset($request->compraCaduca),
                 'compraCaducidad'       => $request->compraCaducidad,
+                'mantenimiento'         => isset($request->mantenimiento),
+                'mantenimientoImporte'  => $request->mantenimientoImporte,
+                'mantenimientoMoneda'   => $request->mantenimientoMoneda,
+                'venta'                 => isset($request->venta),
+                'ventaPrecio'           => $request->ventaPrecio,
+                'ventaMoneda'           => $request->ventaMoneda,
+                'ventaNegociable'       => isset($request->ventaNegociable),
+                'ventaOcultarImporte'   => isset($request->ventaOcultarImporte),
+                'renta'                 => isset($request->renta),
+                'rentaPrecio'           => $request->rentaPrecio,
+                'rentaMoneda'           => $request->rentaMoneda,
+                'rentaNegociable'       => isset($request->rentaNegociable),
+                'status'                => $request->status,
+                'telContacto'           => $request->telContacto,
+                'cualSemanaFija'        => $request->cualSemanaFija,
+                'cuantosPuntos'         => $request->cuantosPuntos,
+                'cuantasNoches'         => $request->cuantasNoches,
+                'cualTemporadaflotante' => $request->cualTemporadaflotante,
+                'updated'               => $request->updated,
+                'created'               => $request->created,
+                'ubicadoEn'             => $request->ubicadoEn,
+                'metodoPago'            => $request->metodoPago,
+                'numCamas'              => $request->numCamas,
+                'localidadNombre'       => $request->localidadNombre,
+                'paisNombre'            => $request->paisNombre,
+                'enlace_url'            => $request->enlace_url,
+                'idPerson'              => $idPerson,
+                
+            ],
+            'headers' => [
+                'Authorization' => $ACCESS_TOKEN
+            ]
+        ]);
+    }
+
+     /**
+     * Updates a specific Membresia by HTTP Request 
+     * Method: PUT
+     * URI: http://0.0.0.0:3000/api/Membresia/{id}
+     */
+    public static function edit($client, $request, $idPerson, $ACCESS_TOKEN)
+    {
+        return $client->request('PUT', 'Membresia', [
+            'form_params' => [
+                'titulo'                => $request->titulo,
+                'title'                 => $request->title,
+                'clubNombre'            => $request->clubNombre,
+                'clubUrl'               => $request->clubUrl,
+                'semanaTipo'            => $request->semanaTipo,
+                'descripcion'           => $request->descripcion,
+                'description'           => $request->description,
+                'sala'                  => isset($request->sala),
+                'dormitorios'           => $request->dormitorios,
+                'lockOff'               => isset($request->lockOff),
+                'tipoInmueble'          => $request->tipoInmueble,
+                'banosCompletos'        => $request->banosCompletos,
+                'banosMedios'           => $request->banosMedios,
+                'tipoCocina'            => $request->tipoCocina,
+                'maxOcupantes'          => $request->maxOcupantes,
+                'maxPrivacidad'         => $request->maxPrivacidad,
+                'frecSemanasPorAnio'    => $request->frecSemanasPorAnio,
+                'frecCadaAnios'         => $request->frecCadaAnios,
+                'compraFecha'           => $request->compraFecha,
+                'ocultarFecha'          => isset($request->ocultarFecha),
+                'compraCaduca'          => isset($request->compraCaduca),
+                'compraCaducidad'       => $request->compraCaducidad,
+                'mantenimiento'         => isset($request->mantenimiento),
                 'mantenimientoImporte'  => $request->mantenimientoImporte,
                 'mantenimientoMoneda'   => $request->mantenimientoMoneda,
                 'venta'                 => isset($request->venta),
