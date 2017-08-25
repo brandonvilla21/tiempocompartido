df9fb72e4ad1d1856b691be84d0683764b3c03cd
Brandon Villa <bornofos@gmail.com>
2017-08-25 14:36:18 -0500

    New models

---
+++ app/Destacado.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Destacado extends Model
+{
+    /**
+     * Get all Destacados by HTTP Request 
+     * Method: GET
+     * URI: http://0.0.0.0:3000/api/Destacados
+     */
+     public static function getAll($client)
+     {
+         return $client->request('GET', 'Destacados', [
+             'headers' => [
+                 'Accept' => 'application/json'
+             ]
+         ]);
+     }
+}
