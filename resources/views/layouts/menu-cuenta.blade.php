7c41daf5b4c44f3b07ce7b40ffda31bbfaed43ac
Brandon Villa <bornofos@gmail.com>
2017-09-26 16:03:52 -0500

    Active list

--- resources/views/layouts/menu-cuenta.blade.php
+++ resources/views/layouts/menu-cuenta.blade.php
@@ -2,13 +2,13 @@
     Mis datos
 </h1>
 <div class="list-group">
-    <a class="list-group-item active" style="color:white;">
+    <a class="list-group-item tiempocompartido-list">
         Tus opciones en tiempo compartido
     </a>
     
-    <a href="/mis-datos" class="list-group-item list-group-item-action {{ \Request::is('mis-datos') ? 'active': ''}}"><i class="fa fa-user"></i> Mis datos</a>
-    <a href="/mis-membresias" class="list-group-item list-group-item-action {{ \Request::is('mis-membresias') ? 'active': ''}}"><i class="fa fa-home"></i> Mis membresias</a>
-    <a href="/mis-favoritos" class="list-group-item list-group-item-action {{ \Request::is('mis-favoritos') ? 'active': ''}}"><i class="fa fa-heart"></i> Mi lista de favoritos</a>
-    <a href="/mis-mensajes" class="list-group-item list-group-item-action {{ \Request::is('mis-mensajes') ? 'active': ''}}"><i class="fa fa-envelope"></i> Mis mensajes</a>
-    <a href="/cambiar-contrasena" class="list-group-item list-group-item-action {{ \Request::is('cambiar-contrasena') ? 'active': ''}}"><i class="fa fa-key"></i> Cambiar mi contraseña</a>
+    <a href="/mis-datos" class="list-group-item list-group-item-action tiemplocompartido-list-active {{ \Request::is('mis-datos') ? 'active': ''}}"><i class="fa fa-user"></i> Mis datos</a>
+    <a href="/mis-membresias" class="list-group-item list-group-item-action tiemplocompartido-list-active {{ \Request::is('mis-membresias') ? 'active': ''}}"><i class="fa fa-home"></i> Mis membresias</a>
+    <a href="/mis-favoritos" class="list-group-item list-group-item-action tiemplocompartido-list-active {{ \Request::is('mis-favoritos') ? 'active': ''}}"><i class="fa fa-heart"></i> Mi lista de favoritos</a>
+    <a href="/mis-mensajes" class="list-group-item list-group-item-action tiemplocompartido-list-active {{ \Request::is('mis-mensajes') ? 'active': ''}}"><i class="fa fa-envelope"></i> Mis mensajes</a>
+    <a href="/cambiar-contrasena" class="list-group-item list-group-item-action tiemplocompartido-list-active {{ \Request::is('cambiar-contrasena') ? 'active': ''}}"><i class="fa fa-key"></i> Cambiar mi contraseña</a>
 </div>
